ab2f5263b998eb506e3754bffa4a5489c9a3872e
John Pouliezos <jpouliezos@gmail.com>
2022-03-23 16:15:02 +0200

    Replace method parameters of type object with typed paramater

--- packages/client/src/generate.ts
+++ packages/client/src/generate.ts
@@ -1,6 +1,7 @@
+import { prototype } from 'form-data';
 import { generate, Indent } from 'openapi-typescript-codegen'
 import * as path from 'path'
-import { Project, MethodDeclarationStructure, Scope, TypeAliasDeclarationStructure, OptionalKind, ImportDeclarationStructure, ExportDeclarationStructure } from "ts-morph";
+import { Project, MethodDeclarationStructure, Scope, TypeAliasDeclarationStructure, OptionalKind, ImportDeclarationStructure, ExportDeclarationStructure, MethodDeclaration } from "ts-morph";
 
 const generatedClientNameClassName = 'Nwc'
 const projectPath = './src/nwc'
@@ -35,10 +36,15 @@ function addCachingToNWCClient(project: Project) {
         const initialStructure = methodDefinition.getStructure() as MethodDeclarationStructure;
         methodDefinition.setScope(Scope.Private);
         methodDefinition.rename(methodDefinition.getName() + "Cancelable");
+
+        /* Handle return types */
+
         let returnType: string | undefined = initialStructure.returnType?.toString().trim();
         if (returnType) {
             let arg = returnType.substring(0, returnType.length - 1).replace('CancelablePromise<', '').split('\r\n').join('')
             if (arg.startsWith('{')) {
+
+
                 arg = `{${arg.replace('{', '').trim()}`
                 const typeName = `${initialStructure.name}ResponseType`
                 const existingTypeDeclaration = additionalTypes.find((type) => type.name === arg)
@@ -52,7 +58,6 @@ function addCachingToNWCClient(project: Project) {
                 arg = typeName
                 methodDefinition.setReturnType(`CancelablePromise<${arg}>`)
             }
-
             returnType = `Promise<${arg}>`
         }
 
@@ -63,6 +68,24 @@ function addCachingToNWCClient(project: Project) {
             newMethod.setReturnType(returnType);
         }
 
+        for (const parameter of newMethod.getParameters()) {
+            if (parameter.getType().isObject()) {
+                const typeName = `${newMethod.getName()}${parameter.getName().charAt(0).toUpperCase()}${parameter.getName().slice(1)}`
+                const existingTypeDeclaration = additionalTypes.find((type) => type.name === typeName)
+                if (!existingTypeDeclaration) {
+                    const parameterText = initialStructure.parameters?.find((p) => p.name === parameter.getName().toString())?.type?.toString()
+                    if (parameterText)
+                        additionalTypes.push({
+                            name: typeName,
+                            type: parameterText,
+                            isExported: true
+                        })
+                }
+                parameter.setType(typeName)
+                methodDefinition.getParameter(parameter.getName())?.setType(typeName)
+            }
+        }
+
         newMethod.setBodyText(`return this.${methodDefinition.getName()}(${methodDefinition.getParameters().map((p) => p.getName()).join(', ').trim()}).then((response) => Promise.resolve(response)).catch((error: ApiError) => Promise.reject(error))`);
         newMethod.addDecorator({ name: "Cacheable", arguments: [] });
         const docs = methodDefinition.getJsDocs();
@@ -70,22 +93,24 @@ function addCachingToNWCClient(project: Project) {
             doc.remove();
         }
     }
+
     if (additionalTypes.length > 0) {
         const additionalTypesFile = project.createSourceFile(path.join(projectPath, 'models/additionalTypes.ts'), undefined, { overwrite: true })
         additionalTypesFile.addTypeAliases(additionalTypes)
         additionalTypesFile.fixMissingImports()
-        const foundNamedImports = additionalTypesFile.getImportDeclarations()[0]
-        const typeNames = foundNamedImports.getNamedImports().map((namedImport) => namedImport.getName())
-        const importDeclarations = typeNames.map((typeName) => {
-            return {
-                moduleSpecifier: `./${typeName}`,
-                namedImports: [typeName]
-            } as OptionalKind<ImportDeclarationStructure>
-        })
-        additionalTypesFile.insertImportDeclarations(0, importDeclarations)
+        // const foundNamedImports = additionalTypesFile.getImportDeclarations()[0]
+
+        // const typeNames = foundNamedImports.getNamedImports().map((namedImport) => namedImport.getName())
+        // const importDeclarations = typeNames.map((typeName) => {
+        //     return {
+        //         moduleSpecifier: `./${typeName}`,
+        //         namedImports: [typeName]
+        //     } as OptionalKind<ImportDeclarationStructure>
+        // })
+        // additionalTypesFile.insertImportDeclarations(0, importDeclarations)
+        // foundNamedImports.remove()
 
         additionalTypesFile.insertStatements(0, ['/* istanbul ignore file */', '/* tslint:disable */', '/* eslint-disable */'])
-        foundNamedImports.remove()
         serviceClassFile.fixMissingImports()
         const indexFile = project.getSourceFileOrThrow(path.join(projectPath, 'index.ts'))
         const exportDeclarations = additionalTypes.map((type) => {
