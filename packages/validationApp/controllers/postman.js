f3900514789c0605c13a2821c5c611ec6841e3da
John Pouliezos <jpouliezos@gmail.com>
2022-07-06 17:41:28 +0300

    Moved validation app to different repo

--- packages/validationApp/controllers/postman.js
+++
@@ -1,8 +0,0 @@
-const sdk = require('@nwc-sdk/client')
-
-const run = async(req, res, next) => {
-    res.json(sdk.Specifications.postman())
-
-}
-
-module.exports = run
--- packages/validationApp/controllers/swagger.js
+++
@@ -1,7 +0,0 @@
-const sdk = require('@nwc-sdk/client')
-
-const run = async(req, res, next) => {
-    res.json(sdk.Specifications.swagger())
-}
-
-module.exports = run
