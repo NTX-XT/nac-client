e7f304da1d987aada2082946e45a3d538c0f464c
John Pouliezos <jpouliezos@gmail.com>
2022-07-06 16:13:41 +0300

    Moved nwc-explorer to a separate repo

--- packages/explorer/src/settings.ts
+++
@@ -1,8 +0,0 @@
-import { ClientCredentials } from '@nwc-sdk/client'
-import * as vscode from 'vscode'
-import { SHARE_ENV } from 'worker_threads'
-import { Informer } from './informer'
-
-export interface IConfiguration {
-	connections: ClientCredentials[]
-}
